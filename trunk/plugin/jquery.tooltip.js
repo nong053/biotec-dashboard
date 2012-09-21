34dc7b88fe34db93653a619045c2d99236cd3e27
nattarat@goingjesse.com <nattarat@goingjesse.com@944b88f3-2a8e-9964-b619-0ade4b84819f>
2012-09-21 02:21:01 +0000

    tooltip hover summary dashboard

--- trunk/plugin/jquery.tooltip.js
+++ trunk/plugin/jquery.tooltip.js
@@ -1,21 +1,27 @@
 jQuery.fn.tooltip = function(option){
 	//alert("hello plugin");
 	var optionDefault={
-		txt:"kpi expand",
+		//id:this.id,
+		//txt:$(this).text(),
+		txt:$("Empty String!"),
 		color:"black"
 	}
 	var ex = jQuery.extend(optionDefault,option);
 
 return	$(this).hover(function(e){
+	var thisID = this.id;
 	var $left = e.pageX+10;
 	var $top = e.pageY+10;
-	$("#tooltip").empty()
-	.append(ex.txt)
-	.css({"color":ex.color,"left":$left,"top":$top})
-	.show("1000");
-
+	var classT = ".tootip#"+thisID;
+	$(classT).css({"left":$left+"px","top":$top+"px"}).fadeIn();
+	//$(classT).empty()
+	//	.append(ex.txt)
+	//.css({"color":ex.color,"left":$left,"top":$top})
+	//	.show("1000");
+	//console.log(thisID);
+	//console.log($(classT).text());
 	},function(){
-	$("#tooltip").hide();
+	$(".tootip").hide();
 	});
 	
 }
