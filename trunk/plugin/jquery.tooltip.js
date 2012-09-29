4788e9b80a3639a3d815fbcd440bf707c895faaa
kosit@goingjesse.com <kosit@goingjesse.com@944b88f3-2a8e-9964-b619-0ade4b84819f>
2012-09-29 08:03:35 +0000

    edit

--- trunk/plugin/jquery.tooltip.js
+++ trunk/plugin/jquery.tooltip.js
@@ -13,13 +13,9 @@ return	$(this).hover(function(e){
 	var $left = e.pageX+10;
 	var $top = e.pageY+10;
 	var classT = ".tootip#"+thisID;
-	$(classT).css({"left":$left+"px","top":$top+"px"}).fadeIn();
-	//$(classT).empty()
-	//	.append(ex.txt)
-	//.css({"color":ex.color,"left":$left,"top":$top})
-	//	.show("1000");
-	//console.log(thisID);
-	//console.log($(classT).text());
+		if($.trim($(classT).text())!=""){
+			$(classT).css({"left":$left+"px","top":$top+"px"}).show();
+		}
 	},function(){
 	$(".tootip").hide();
 	});
